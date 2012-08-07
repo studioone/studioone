d772e74909cc1552feb4dc575a9cdf30e7e54987
bleen <git.119x@bleen.net>
2012-08-06 21:17:53 -0400

    Theming examples page

--- docroot/sites/all/modules/custom/studio_programs/js/studio_programs.view_nav_and_previews.js
+++ docroot/sites/all/modules/custom/studio_programs/js/studio_programs.view_nav_and_previews.js
@@ -2,17 +2,17 @@
 (function ($, Drupal, window, document, undefined) {
 
   $(function () {
-    
+
     Drupal.settings.studio_programs = Drupal.settings.studio_programs || {};
     Drupal.settings.studio_programs.header_nav_view = Drupal.settings.studio_programs.header_nav_view || '';
-    
+
     //var view = $('.view-program-examples');
     var view = $('.' + Drupal.settings.studio_programs.header_nav_view)
-    
+
     if (view.length) {
-      
+
       var view_all_items = $('.program-examples-view-item-container', view);
-      
+
       function view_change(section_id) {
         if (section_id == 'all') {
           view_all_items.fadeTo(650, 1);
@@ -22,21 +22,21 @@
           view_section_items[ section_id ].fadeTo(450, 1);
         }
       }
-      
+
       // Attach click handlers to view header navigation anchors
       var view_section_items = {};
-      
+
       $('.program-examples-view-header-anchor', view).each(function (i) {
         this_element = $(this);
         this_section_id = this_element.attr('href').replace('#section-', '');
         view_section_items[ this_section_id ] = $('.program-examples-view-section-' + this_section_id + '-item', view);
         this_element.click(function() { view_change( $(this).attr('href').replace('#section-', '') ); });
       });
-      
+
       // Attach click handlers to main item thumbnails to display preview
       var preview_showing = false;
       var preview_fading_out = false;
-      
+
       function show_preview(element) {
         if (!preview_showing && !preview_fading_out) {
           preview_showing = true;
@@ -49,13 +49,9 @@
           $(element).find('.program-examples-view-item-preview').fadeOut(200, function () { preview_fading_out = false; preview_showing = false; });
         }
       }
-      
-      view_all_items
-        .mouseover(function () { show_preview(this); })
-        .click(function () { show_preview(this); return false; });
-      
-      //.mouseout(function () { hide_preview(this); });
-      
+
+      view_all_items.click(function () { show_preview(this); return false; });
+
       // Attach click handlers to preview close X's
       $('.program-examples-view-preview-close').click(function () {
         hide_preview($(this).parent().parent());
