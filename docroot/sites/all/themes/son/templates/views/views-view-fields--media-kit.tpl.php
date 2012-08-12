f6bc2b175b1d5b7ee9731ef0b18b6df1cef0ce10
Tim V <breathingrock@gmail.com>
2012-08-11 20:59:36 -0600

    Adding headers to media kit view.

--- docroot/sites/all/themes/son/templates/views/views-view-fields--media-kit.tpl.php
+++ docroot/sites/all/themes/son/templates/views/views-view-fields--media-kit.tpl.php
@@ -41,7 +41,7 @@ $field_document_file_data = array_shift(field_get_items('node', $node, 'field_do
     $file_simple_mime =  isset($file_simple_mime_map[ $field_document_file_data['filemime'] ]) ?
       $file_simple_mime_map[ $field_document_file_data['filemime'] ] : 'FILE';
     $icon_filename = $file_simple_mime != 'FILE' ? $file_simple_mime .'.ico' : '_Documents.ico';
-    $file_icon_src = drupal_get_path('theme', 'son') .'/images/FineFiles/'. $icon_filename;
+    $file_icon_src = '/'. drupal_get_path('theme', 'son') .'/images/FineFiles/'. $icon_filename;
   ?>
   <div class="media-kit-file-container">
     <img src="<?php echo $file_icon_src; ?>" class="media-kit-file-icon" /><a href="<?php echo $file_href; ?>" class="media-kit-file-link">Download <?php echo strtoupper($file_simple_mime); ?></a>
