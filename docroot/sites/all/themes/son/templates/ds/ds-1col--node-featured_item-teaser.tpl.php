03a532deb219aed3150c6ae91d05d59762196403
bleen <git.119x@bleen.net>
2013-04-12 17:14:59 -0400

    Fixing the dynamic lead so that only the image transitions and removing the "arrow" form teh dynamic lead pager

---
+++ docroot/sites/all/themes/son/templates/ds/ds-1col--node-featured_item-teaser.tpl.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 2 column template (customized).
+ */
+?>
+<div class="ds-1col <?php print $classes;?> clearfix">
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+  <?php
+    // Customized featured image.
+    $custom_image_options = array(
+      'style_name' => $content['field_featured_item_image'][0]['#image_style'],
+      'path' => $content['field_featured_item_image'][0]['#item']['uri'],
+      'alt' => $content['field_featured_item_image'][0]['#item']['alt'],
+      'title' => $content['field_featured_item_image'][0]['#item']['title'],
+      'width' => $content['field_featured_item_image'][0]['#item']['image_dimensions']['width'],
+      'height' => $content['field_featured_item_image'][0]['#item']['image_dimensions']['height'],
+    );
+  ?>
+  <a href="<?php echo $field_featured_item_url[ $language ][0]['safe_value']; ?>"><?php echo theme('image_style', $custom_image_options); ?></a>
+
+</div>
+
+
