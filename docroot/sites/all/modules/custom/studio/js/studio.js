31d2b31dbee3df0cfbc53631e667542b371497bd
bleen <git.119x@bleen.net>
2013-05-25 11:21:11 -0400

    Adding a "watch video" block to the homepage

---
+++ docroot/sites/all/modules/custom/studio/js/studio.js
@@ -0,0 +1,24 @@
+Drupal.settings.studio = Drupal.settings.studio || {};
+
+function onYouTubeIframeAPIReady() {
+  Drupal.settings.studio.player = new YT.Player('studio-youtube-player', {
+    height: '146',
+    width: '260',
+    events: {
+      'onReady': onPlayerReady
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  jQuery('.field-name-field-youtube-path .field-item a').click(function() {
+    videoId = jQuery(this).attr('href').split('=')[1];
+    Drupal.settings.studio.player.loadVideoById({videoId:videoId});
+    return false;
+  });
+
+  // Cue up the first video on load.
+  videoId = jQuery('.field-name-field-youtube-path .field-item:first a').attr('href').split('=')[1];
+  Drupal.settings.studio.player.cueVideoById({videoId:videoId});
+
+}
